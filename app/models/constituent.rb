b425f08ebd4a78764fd03423c1aefa0cf1fd39d0
Shun Liang <shun.liang.public@gmail.com>
2015-11-07 22:50:48 +0000

    Added vote_on method for Constituent model

--- app/models/constituent.rb
+++ app/models/constituent.rb
@@ -7,4 +7,17 @@ class Constituent < ActiveRecord::Base
 
   has_many :votes
   has_many :motions, through: :votes
+
+  def vote_on (motion_id, points)
+    # check if the constituent has voted for the same motion already
+    # if so, update that vote
+    vote = votes.find_by(motion_id: motion_id)
+    if vote.nil?
+      votes << Vote.create(motion_id: motion_id, points: points)
+    else
+      vote.points = points
+      vote.save()
+    end
+  end
+
 end
